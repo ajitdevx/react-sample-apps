9fc27494dcb458796c139d1f9adaa9fd7a8a6043
ajithkumar.alagesan@straive.com <ajithkumar.alagesan@straive.com>
2025-02-14 07:04:23 +0530

    Funfact basic design

---
+++ digital-business-card/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-body-tertiary" data-bs-theme="dark">
+            <div className="container-fluid">
+                <a href="" className="navbar navbar-brand">React Funfacts</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
