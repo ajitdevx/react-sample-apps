36ab739026edca351594b8b32be078088e0b8a09
ajithkumar.alagesan@straive.com <ajithkumar.alagesan@straive.com>
2025-02-16 15:46:28 +0530

    Travel Journal

---
+++ travel-journal/src/data.js
@@ -0,0 +1,39 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-japan-image-url",
+            //psrc: "https://nl.hideproxy.me/go.php?u=5KkDV9rx%2Fbe4ze9ORaWT9jI2Zg2OxjTIE3HWAVLVCYYzhCQzkXsb1G8j0dIO3tgd0SoFerKO0mq%2BP31hu0cl30X3CsdimLFve6dSm6mLpSyVis%2BVD6fakACZB8u81cXrVHjwTYlqWLshrgIjGKQk9EQdM5HIP5nYOlwFXCDYT0U8SLVVmL9mI33oXaC4UNqEUNhhFpxKiCN4bLFdw5%2FDr7YXzQIIjmxSyOSDbI%2B0Q5fY&b=5",
+            alt: "Mount Fuji"
+        },
+        title: "Mount Fuji",
+        country: "Japan",
+        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        dates: "12 Jan, 2021 - 24 Jan, 2021",
+        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-australia-image-url",
+            alt: "Sydney Opera House"
+        },
+        title: "Sydney Opera House",
+        country: "Australia",
+        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        dates: "27 May, 2021 - 8 Jun, 2021",
+        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-norway-image-url",
+            alt: "Geirangerfjord"
+        },
+        title: "Geirangerfjord",
+        country: "Norway",
+        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        dates: "01 Oct, 2021 - 18 Nov, 2021",
+        text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+    },
+]
