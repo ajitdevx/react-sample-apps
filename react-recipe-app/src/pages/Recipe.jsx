423c04f48dba8a106b627a0be388b6e2f1231b7f
ajit <ase.ajithkumar@gmail.com>
2025-02-26 18:57:48 +0530

    recipe

--- react-recipe-app/src/pages/Recipe.jsx
+++ react-recipe-app/src/pages/Recipe.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { recipeService } from "../_services/recipe.service";
 import { RecipeCategory } from "../components/Recipe/RecipeCategory";
 import RecipeList from "../components/Recipe/RecipeList";
+import banner from '../_assets/slide1-1.jpg';
 
 const defaultCategory = {
     categories: [],
@@ -27,30 +28,27 @@ const Recipe = () => {
 
     }, []);
 
-    useEffect(() => {        
+    useEffect(() => {
         recipeService.getRecipesByCategory(category.activeCategoryName).then(data => {
             setRecipes({
                 items: data.meals || [],
             });
         })
-    }, [category.activeCategoryName])
+    }, [category.activeCategoryName]);
+
+    const handleCategoryClick = (item) => {
+        setCategory({ ...category, activeCategoryName: item })
+    }
 
     return (
         <>
             <main className="container">
                 <div className="bg-body-secondary p-5 rounded mt-3">
-                    <h1>Bottom Navbar example</h1>
-                    <p className="lead">
-                        This example is a quick exercise to illustrate how the bottom navbar
-                        works.
-                    </p>
-                    <a
-                        className="btn btn-lg btn-primary"
-                        href="/docs/5.3/components/navbar/"
-                        role="button"
-                    >
-                        View navbar docs »
-                    </a>
+                    <div className="row">
+                        <div className="col-12">
+                            <img src={banner} alt="" className="img-fluid rounded" />
+                        </div>
+                    </div>
                 </div>
                 <div className="container">
                     <div className="d-flex justify-content-center m-3">
@@ -58,7 +56,7 @@ const Recipe = () => {
                             What to <span className="text-warning">Cook</span> ?
                         </h3>
                     </div>
-                    <RecipeCategory category={category} />
+                    <RecipeCategory category={category} handleCategoryClick={handleCategoryClick} />
                 </div>
                 <RecipeList recipes={recipes} />
             </main>
